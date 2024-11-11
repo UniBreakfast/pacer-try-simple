74ee162b0777362fae16cc1fea9171d63a5f8162
UniBreakfast <unibreakfast1@gmail.com>
2024-11-11 16:45:19 +0200

    style confidence counter

--- confidence-hud.js
+++ confidence-hud.js
@@ -8,9 +8,4 @@ function showConfidenceHUD() {
   confidenceOutput.innerText = state.confidence
 
   body.append(confidenceOutput)
-
-  confidenceOutput.style.position = 'fixed'
-  confidenceOutput.style.top = '10px'
-  confidenceOutput.style.right = '10px'
-  confidenceOutput.style.width = 'max-content'
 }
